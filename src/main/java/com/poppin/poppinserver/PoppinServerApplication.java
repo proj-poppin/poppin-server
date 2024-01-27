5fe71fd7c91da082d0814d8335d51ad2a663aeb4
gouyeonch <gouyeonch@naver.com>
2024-01-27 22:15:34 +0900

    Feat : Init Project

---
+++ src/main/java/com/poppin/poppinserver/PoppinServerApplication.java
@@ -0,0 +1,13 @@
+package com.poppin.poppinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoppinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoppinServerApplication.class, args);
+	}
+
+}
