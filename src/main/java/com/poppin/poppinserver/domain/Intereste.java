0f0f71f9abe6649c57aaf7867bc1e1f0ba5461e9
gouyeonch <gouyeonch@naver.com>
2024-02-10 04:42:58 +0900

    feat : interest 복합키 클래스 추가

--- src/main/java/com/poppin/poppinserver/domain/Intereste.java
+++ src/main/java/com/poppin/poppinserver/domain/Intereste.java
@@ -12,15 +12,17 @@ import org.hibernate.annotations.DynamicUpdate;
 @DynamicUpdate
 @Table(name = "intereste")
 public class Intereste {
+    @EmbeddedId
+    private InteresteId id;
 
-    @Id
     @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    @MapsId("userId")
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
-    @Id
     @ManyToOne
-    @JoinColumn(name = "store_id", referencedColumnName = "store_id")
-    private Popup store;
+    @MapsId("popupId")
+    @JoinColumn(name = "popup_id", referencedColumnName = "id")
+    private Popup popup;
 
 }
