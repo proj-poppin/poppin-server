e09b22e386944cf2450196651eefd6a3a62cf9e2
jun6292 <c68254@gmail.com>
2024-02-09 15:38:34 +0900

    Fix: EUserRole - GUEST 권한 추가

--- src/main/java/com/poppin/poppinserver/type/EUserRole.java
+++ src/main/java/com/poppin/poppinserver/type/EUserRole.java
@@ -5,7 +5,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum EUserRole {
     USER("USER", "ROLE_USER"),
-    ADMIN("ADMIN", "ROLE_ADMIN");
+    ADMIN("ADMIN", "ROLE_ADMIN"),
+    GUEST("GUEST", "ROLE_GUEST");
 
     private final String name;
     private final String securityName;
