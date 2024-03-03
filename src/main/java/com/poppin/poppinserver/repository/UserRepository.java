9f3ee3db5961d35acd20639a1f8b96e2aaf3cea4
jun6292 <c68254@gmail.com>
2024-03-03 11:48:00 +0900

    Feat: PasswordUtil - 비밀번호 랜덤 생성 기능 추가

--- src/main/java/com/poppin/poppinserver/repository/UserRepository.java
+++ src/main/java/com/poppin/poppinserver/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.poppin.poppinserver.repository;
 
 import com.poppin.poppinserver.domain.User;
+import com.poppin.poppinserver.type.ELoginProvider;
+import com.poppin.poppinserver.type.EUserRole;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -12,10 +14,17 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByNickname(String email);
+    Optional<User> findById(Long id);
+    Optional<User> findByNickname(String nickname);
 
     @Transactional
     @Modifying(clearAutomatically = true)
     @Query("UPDATE User u SET u.refreshToken = :refreshToken, u.isLogin = :loginStatus WHERE u.email = :email")
     void updateRefreshTokenAndLoginStatus(String email, String refreshToken, boolean loginStatus);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.provider = :eLoginProvider")
+    Optional<User> findByEmailAndELoginProvider(String email, ELoginProvider eLoginProvider);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.role = :role")
+    Optional<User> findByEmailAndRole(String email, EUserRole role);
 }
