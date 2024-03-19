567f4cf898a87394b7ba5f033cd1d0be124e2407
gouyeonch <gouyeonch@naver.com>
2024-03-19 16:43:09 +0900

    Feat : TasteDto fromEntity 정의

--- src/main/java/com/poppin/poppinserver/dto/popup/response/TasteDto.java
+++ src/main/java/com/poppin/poppinserver/dto/popup/response/TasteDto.java
@@ -3,7 +3,9 @@ package com.poppin.poppinserver.dto.popup.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record TasteDto(
         Long id,
@@ -20,4 +22,22 @@ public record TasteDto(
         Boolean kpop,
         Boolean alchol,
         Boolean animalPlant) {
+        public static TasteDto fromEntity(TasteDto tasteDto){
+                return TasteDto.builder()
+                        .id(tasteDto.id)
+                        .fasionBeauty(tasteDto.fasionBeauty)
+                        .character(tasteDto.character)
+                        .foodBeverage(tasteDto.foodBeverage)
+                        .webtoonAni(tasteDto.webtoonAni)
+                        .interiorThings(tasteDto.interiorThings)
+                        .movie(tasteDto.movie)
+                        .musical(tasteDto.musical)
+                        .sports(tasteDto.sports)
+                        .game(tasteDto.game)
+                        .itTech(tasteDto.itTech)
+                        .kpop(tasteDto.kpop)
+                        .alchol(tasteDto.alchol)
+                        .animalPlant(tasteDto.animalPlant)
+                        .build();
+        }
 }
