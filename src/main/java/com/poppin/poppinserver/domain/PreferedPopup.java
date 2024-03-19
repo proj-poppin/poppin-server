eb6baf8e02852fa90a824e5cf4ce64965611a243
gouyeonch <gouyeonch@naver.com>
2024-03-19 15:06:18 +0900

    Feat : PreferedPopup WhoWithPopup 엔티티 정의

---
+++ src/main/java/com/poppin/poppinserver/domain/PreferedPopup.java
@@ -0,0 +1,39 @@
+package com.poppin.poppinserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+@Table(name = "prefered_popup")
+public class PreferedPopup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "popup-id")
+    private Popup popupId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user-id")
+    private Popup userId;
+
+    @Column(name = "market")
+    private Boolean market;
+
+    @Column(name = "display")
+    private Boolean display;
+
+    @Column(name = "experience")
+    private Boolean experience;
+
+    @Column(name = "want_free")
+    private Boolean wantFree;
+}
---
+++ src/main/java/com/poppin/poppinserver/domain/WhoWithPopup.java
@@ -0,0 +1,39 @@
+package com.poppin.poppinserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+@Table(name = "who_with_popup")
+public class WhoWithPopup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "popup-id")
+    private Popup popupId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user-id")
+    private Popup userId;
+
+    @Column(name = "solo")
+    private Boolean solo;
+
+    @Column(name = "with_friend")
+    private Boolean withFriend;
+
+    @Column(name = "with_family")
+    private Boolean withFamily;
+
+    @Column(name = "with_bool")
+    private Boolean withBool;
+}
