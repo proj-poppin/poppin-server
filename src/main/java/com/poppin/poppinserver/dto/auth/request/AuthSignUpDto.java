25d447d07e61147227dcba0c9845800c8182228b
jun6292 <c68254@gmail.com>
2024-03-29 18:34:02 +0900

    fix: 비밀번호 유효성 검사 수정

--- src/main/java/com/poppin/poppinserver/dto/auth/request/AuthSignUpDto.java
+++ src/main/java/com/poppin/poppinserver/dto/auth/request/AuthSignUpDto.java
@@ -8,9 +8,9 @@ public record AuthSignUpDto (
         @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "올바른 이메일 형식이 아닙니다.")
         String email,
 
-        // 영문,숫자 반드시 포함, 특수문자 2개 이상 포함, 8자 이상 20자 이하
+        // 영문/숫자 둘 중 하나 이상 반드시 포함, 특수문자 반드시 포함, 8자 이상
         @NotBlank
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*].*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,20}$", message = "올바른 비밀번호 형식이 아닙니다.")
+        @Pattern(regexp = "^(?=.*[A-Za-z\\d])(?=.*[!@#$%^&*()]).{8,}$", message = "올바른 비밀번호 형식이 아닙니다.")
         String password,
 
         @NotBlank(message = "비밀번호 확인을 입력하세요.")
