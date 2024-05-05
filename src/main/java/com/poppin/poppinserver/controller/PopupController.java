0e9050b6ceec6285f62ddb64c296f312a780d201
gouyeonch <gouyeonch@naver.com>
2024-05-05 22:55:51 +0900

    Refactor : constante url ㅅㅜ정

--- src/main/java/com/poppin/poppinserver/controller/PopupController.java
+++ src/main/java/com/poppin/poppinserver/controller/PopupController.java
@@ -14,6 +14,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
