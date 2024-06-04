4ad3220955d8f7c9da1213b1eed854d6bdce43c5
sakang <rogersddt@naver.com>
2024-06-04 21:45:01 +0900

    Feat: 방문자데이터 수정

--- src/main/java/com/poppin/poppinserver/service/VisitorDataService.java
+++ src/main/java/com/poppin/poppinserver/service/VisitorDataService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -22,14 +23,30 @@ public class VisitorDataService {
 
     public VisitorDataInfoDto getVisitorData(Long popupId){
 
-        Map<String,Object> weekdayAm = visitorDataRepository.findCongestionRatioByPopupId(popupId, EVisitDate.fromValue("평일 오전").toString());
-        Map<String,Object> weekdayPm = visitorDataRepository.findCongestionRatioByPopupId(popupId, EVisitDate.fromValue("평일 오후").toString());
-        Map<String,Object> weekendAm = visitorDataRepository.findCongestionRatioByPopupId(popupId, EVisitDate.fromValue("주말 오전").toString());
-        Map<String,Object> weekendPm = visitorDataRepository.findCongestionRatioByPopupId(popupId, EVisitDate.fromValue("주말 오후").toString());
+        Map<String,Object> weekdayAm = checkAndInitialize(visitorDataRepository.findCongestionRatioByPopupId(popupId, EVisitDate.fromValue("평일 오전").toString()));
+        Map<String,Object> weekdayPm = checkAndInitialize(visitorDataRepository.findCongestionRatioByPopupId(popupId, EVisitDate.fromValue("평일 오후").toString()));
+        Map<String,Object> weekendAm = checkAndInitialize(visitorDataRepository.findCongestionRatioByPopupId(popupId, EVisitDate.fromValue("주말 오전").toString()));
+        Map<String,Object> weekendPm = checkAndInitialize(visitorDataRepository.findCongestionRatioByPopupId(popupId, EVisitDate.fromValue("주말 오후").toString()));
         Optional<Integer> satisfaction = visitorDataRepository.satisfactionRate(popupId, ESatisfaction.fromValue("만족").toString());
 
         VisitorDataInfoDto visitorDataDto = VisitorDataInfoDto.fromEntity(weekdayAm, weekdayPm, weekendAm, weekendPm, satisfaction);
 
         return visitorDataDto;
+    }
+
+    private Map<String, Object> checkAndInitialize(Map<String, Object> result) {
+        if (result == null) {
+            result = new HashMap<>();
+            result.put("congestionRate", "여유");  // 기본 값 설정
+            result.put("congestionRatio", 0);
+        } else {
+            if (result.get("congestionRate") == null) {
+                result.put("congestionRate", "여유");  // 기본 값 설정
+            }
+            if (result.get("congestionRatio") == null) {
+                result.put("congestionRatio", 0);
+            }
+        }
+        return result;
     }
 }
