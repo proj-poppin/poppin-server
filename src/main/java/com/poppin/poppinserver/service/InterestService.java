21a1b4eb27589e80ea3be6ff0ecc5b79f714872e
sakang <rogersddt@naver.com>
2024-07-04 01:45:05 +0900

    Fix : 관심팝업 등록/취소 param 변경

--- src/main/java/com/poppin/poppinserver/service/InterestService.java
+++ src/main/java/com/poppin/poppinserver/service/InterestService.java
@@ -1,6 +1,7 @@
 package com.poppin.poppinserver.service;
 
 import com.poppin.poppinserver.domain.*;
+import com.poppin.poppinserver.dto.interest.request.InterestRequestDto;
 import com.poppin.poppinserver.dto.interest.response.InterestDto;
 import com.poppin.poppinserver.exception.CommonException;
 import com.poppin.poppinserver.exception.ErrorCode;
@@ -22,16 +23,16 @@ public class InterestService {
 
     private final FCMService fcmService;
     @Transactional // 쿼리 5번 날라감. 최적화 필요
-    public InterestDto userAddInterest(Long popupId, Long userId, String token){
+    public InterestDto userAddInterest(Long userId, InterestRequestDto requestDto){
         //중복검사
-        interestRepository.findByUserIdAndPopupId(userId, popupId)
+        interestRepository.findByUserIdAndPopupId(userId, requestDto.popupId())
                 .ifPresent(interest -> {
                     throw new CommonException(ErrorCode.DUPLICATED_INTEREST);
                 });
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
-        Popup popup = popupRepository.findById(popupId)
+        Popup popup = popupRepository.findById(requestDto.popupId())
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_POPUP));
 
         Interest interest = Interest.builder()
@@ -44,7 +45,7 @@ public class InterestService {
         popup.addInterestCnt();
 
         /*알림 구독*/
-        String fcmToken = token;
+        String fcmToken = requestDto.fcmToken();
 
         fcmService.fcmAddTopic(fcmToken, popup, EPopupTopic.MAGAM);
         fcmService.fcmAddTopic(fcmToken, popup, EPopupTopic.OPEN);
@@ -53,15 +54,17 @@ public class InterestService {
         return InterestDto.fromEntity(interest,user,popup);
     }
 
-    public Boolean removeInterest(Long userId, Long popupId, String fcmToken){
-        Interest interest = interestRepository.findByUserIdAndPopupId(userId, popupId)
+    public Boolean removeInterest(Long userId,InterestRequestDto requestDto){
+        Interest interest = interestRepository.findByUserIdAndPopupId(userId, requestDto.popupId())
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_RESOURCE));
 
         interestRepository.delete(interest);
 
-//        /*FCM 구독취소*/
-        Popup popup = popupRepository.findById(popupId)
+        /*FCM 구독취소*/
+        Popup popup = popupRepository.findById(requestDto.popupId())
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_POPUP));
+
+        String fcmToken = requestDto.fcmToken();
 
         fcmService.fcmRemoveTopic(fcmToken,popup, EPopupTopic.MAGAM);
         fcmService.fcmRemoveTopic(fcmToken,popup, EPopupTopic.OPEN);
