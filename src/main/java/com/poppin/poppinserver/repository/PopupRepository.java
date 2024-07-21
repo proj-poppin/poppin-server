63c2c397256c48a1e3adf7004c998af006e17310
Guyeon Chung <gouyeonch@naver.com>
2024-07-21 13:23:27 +0900

    Refactor : 검색 로직 중복 허용

--- src/main/java/com/poppin/poppinserver/repository/PopupRepository.java
+++ src/main/java/com/poppin/poppinserver/repository/PopupRepository.java
@@ -75,23 +75,25 @@ public interface PopupRepository extends JpaRepository<Popup, Long>, JpaSpecific
             "WHERE bp.popup_id IS NULL " +
             "AND (:text IS NULL OR :text = '' OR MATCH(p.name, p.introduce) AGAINST (:text IN BOOLEAN MODE)) " +
             "AND p.operation_status = :oper " +
-            "AND (:fashionBeauty IS NULL OR tp.fashion_beauty = :fashionBeauty) " +
-            "AND (:characters IS NULL OR tp.characters = :characters) " +
-            "AND (:foodBeverage IS NULL OR tp.food_beverage = :foodBeverage) " +
-            "AND (:webtoonAni IS NULL OR tp.webtoon_ani = :webtoonAni) " +
-            "AND (:interiorThings IS NULL OR tp.interior_things = :interiorThings) " +
-            "AND (:movie IS NULL OR tp.movie = :movie) " +
-            "AND (:musical IS NULL OR tp.musical = :musical) " +
-            "AND (:sports IS NULL OR tp.sports = :sports) " +
-            "AND (:game IS NULL OR tp.game = :game) " +
-            "AND (:itTech IS NULL OR tp.it_tech = :itTech) " +
-            "AND (:kpop IS NULL OR tp.kpop = :kpop) " +
-            "AND (:alcohol IS NULL OR tp.alcohol = :alcohol) " +
-            "AND (:animalPlant IS NULL OR tp.animal_plant = :animalPlant) " +
-            "AND (:market IS NULL OR pp.market = :market) " +
-            "AND (:display IS NULL OR pp.display = :display) " +
-            "AND (:experience IS NULL OR pp.experience = :experience)" +
-            "AND (:etc IS NULL OR tp.etc = :etc)",
+            "AND (" +
+            "(:fashionBeauty IS NULL OR tp.fashion_beauty = :fashionBeauty) " +
+            "OR (:characters IS NULL OR tp.characters = :characters) " +
+            "OR (:foodBeverage IS NULL OR tp.food_beverage = :foodBeverage) " +
+            "OR (:webtoonAni IS NULL OR tp.webtoon_ani = :webtoonAni) " +
+            "OR (:interiorThings IS NULL OR tp.interior_things = :interiorThings) " +
+            "OR (:movie IS NULL OR tp.movie = :movie) " +
+            "OR (:musical IS NULL OR tp.musical = :musical) " +
+            "OR (:sports IS NULL OR tp.sports = :sports) " +
+            "OR (:game IS NULL OR tp.game = :game) " +
+            "OR (:itTech IS NULL OR tp.it_tech = :itTech) " +
+            "OR (:kpop IS NULL OR tp.kpop = :kpop) " +
+            "OR (:alcohol IS NULL OR tp.alcohol = :alcohol) " +
+            "OR (:animalPlant IS NULL OR tp.animal_plant = :animalPlant) " +
+            "OR (:etc IS NULL OR tp.etc = :etc)) " +
+            "AND ( " +
+            "OR (:market IS NULL OR pp.market = :market) " +
+            "OR (:display IS NULL OR pp.display = :display) " +
+            "OR (:experience IS NULL OR pp.experience = :experience))",
             countQuery = "SELECT COUNT(*) FROM popups p " +
                     "LEFT JOIN prefered_popup pp ON p.prefered_id = pp.id " +
                     "LEFT JOIN taste_popup tp ON p.taste_id = tp.id " +
@@ -99,31 +101,34 @@ public interface PopupRepository extends JpaRepository<Popup, Long>, JpaSpecific
                     "WHERE bp.popup_id IS NULL " +
                     "AND (:text IS NULL OR :text = '' OR MATCH(p.name, p.introduce) AGAINST (:text IN BOOLEAN MODE)) " +
                     "AND p.operation_status = :oper " +
-                    "AND (:fashionBeauty IS NULL OR tp.fashion_beauty = :fashionBeauty) " +
-                    "AND (:characters IS NULL OR tp.characters = :characters) " +
-                    "AND (:foodBeverage IS NULL OR tp.food_beverage = :foodBeverage) " +
-                    "AND (:webtoonAni IS NULL OR tp.webtoon_ani = :webtoonAni) " +
-                    "AND (:interiorThings IS NULL OR tp.interior_things = :interiorThings) " +
-                    "AND (:movie IS NULL OR tp.movie = :movie) " +
-                    "AND (:musical IS NULL OR tp.musical = :musical) " +
-                    "AND (:sports IS NULL OR tp.sports = :sports) " +
-                    "AND (:game IS NULL OR tp.game = :game) " +
-                    "AND (:itTech IS NULL OR tp.it_tech = :itTech) " +
-                    "AND (:kpop IS NULL OR tp.kpop = :kpop) " +
-                    "AND (:alcohol IS NULL OR tp.alcohol = :alcohol) " +
-                    "AND (:animalPlant IS NULL OR tp.animal_plant = :animalPlant) " +
-                    "AND (:market IS NULL OR pp.market = :market) " +
-                    "AND (:display IS NULL OR pp.display = :display) " +
-                    "AND (:experience IS NULL OR pp.experience = :experience)" +
-                    "AND (:etc IS NULL OR tp.etc = :etc)",
+                    "AND (" +
+                    "(:fashionBeauty IS NULL OR tp.fashion_beauty = :fashionBeauty) " +
+                    "OR (:characters IS NULL OR tp.characters = :characters) " +
+                    "OR (:foodBeverage IS NULL OR tp.food_beverage = :foodBeverage) " +
+                    "OR (:webtoonAni IS NULL OR tp.webtoon_ani = :webtoonAni) " +
+                    "OR (:interiorThings IS NULL OR tp.interior_things = :interiorThings) " +
+                    "OR (:movie IS NULL OR tp.movie = :movie) " +
+                    "OR (:musical IS NULL OR tp.musical = :musical) " +
+                    "OR (:sports IS NULL OR tp.sports = :sports) " +
+                    "OR (:game IS NULL OR tp.game = :game) " +
+                    "OR (:itTech IS NULL OR tp.it_tech = :itTech) " +
+                    "OR (:kpop IS NULL OR tp.kpop = :kpop) " +
+                    "OR (:alcohol IS NULL OR tp.alcohol = :alcohol) " +
+                    "OR (:animalPlant IS NULL OR tp.animal_plant = :animalPlant) " +
+                    "OR (:etc IS NULL OR tp.etc = :etc)) " +
+                    "AND ( " +
+                    "OR (:market IS NULL OR pp.market = :market) " +
+                    "OR (:display IS NULL OR pp.display = :display) " +
+                    "OR (:experience IS NULL OR pp.experience = :experience))",
             nativeQuery = true)
     Page<Popup> findByTextInNameOrIntroduceByBlackList(String text, Pageable pageable,
-                                            Boolean market, Boolean display, Boolean experience,
-                                            Boolean fashionBeauty, Boolean characters, Boolean foodBeverage,
-                                            Boolean webtoonAni, Boolean interiorThings, Boolean movie,
-                                            Boolean musical, Boolean sports, Boolean game,
-                                            Boolean itTech, Boolean kpop, Boolean alcohol,
-                                            Boolean animalPlant, Boolean etc, String oper, Long userId);
+                                                       Boolean market, Boolean display, Boolean experience,
+                                                       Boolean fashionBeauty, Boolean characters, Boolean foodBeverage,
+                                                       Boolean webtoonAni, Boolean interiorThings, Boolean movie,
+                                                       Boolean musical, Boolean sports, Boolean game,
+                                                       Boolean itTech, Boolean kpop, Boolean alcohol,
+                                                       Boolean animalPlant, Boolean etc, String oper, Long userId);
+
 
     //비로그인 팝업 검색
     @Query(value = "SELECT p.* FROM popups p " +
@@ -131,45 +136,49 @@ public interface PopupRepository extends JpaRepository<Popup, Long>, JpaSpecific
             "LEFT JOIN taste_popup tp ON p.taste_id = tp.id " +
             "WHERE (:text IS NULL OR :text = '' OR MATCH(p.name, p.introduce) AGAINST (:text IN BOOLEAN MODE)) " +
             "AND p.operation_status = :oper  " +
-            "AND (:fashionBeauty IS NULL OR tp.fashion_beauty = :fashionBeauty) " +
-            "AND (:characters IS NULL OR tp.characters = :characters) " +
-            "AND (:foodBeverage IS NULL OR tp.food_beverage = :foodBeverage) " +
-            "AND (:webtoonAni IS NULL OR tp.webtoon_ani = :webtoonAni) " +
-            "AND (:interiorThings IS NULL OR tp.interior_things = :interiorThings) " +
-            "AND (:movie IS NULL OR tp.movie = :movie) " +
-            "AND (:musical IS NULL OR tp.musical = :musical) " +
-            "AND (:sports IS NULL OR tp.sports = :sports) " +
-            "AND (:game IS NULL OR tp.game = :game) " +
-            "AND (:itTech IS NULL OR tp.it_tech = :itTech) " +
-            "AND (:kpop IS NULL OR tp.kpop = :kpop) " +
-            "AND (:alcohol IS NULL OR tp.alcohol = :alcohol) " +
-            "AND (:animalPlant IS NULL OR tp.animal_plant = :animalPlant) " +
-            "AND (:market IS NULL OR pp.market = :market) " +
-            "AND (:display IS NULL OR pp.display = :display) " +
-            "AND (:experience IS NULL OR pp.experience = :experience)" +
-            "AND (:etc IS NULL OR tp.etc = :etc)",
+            "AND (" +
+            "(:fashionBeauty IS NULL OR tp.fashion_beauty = :fashionBeauty) " +
+            "OR (:characters IS NULL OR tp.characters = :characters) " +
+            "OR (:foodBeverage IS NULL OR tp.food_beverage = :foodBeverage) " +
+            "OR (:webtoonAni IS NULL OR tp.webtoon_ani = :webtoonAni) " +
+            "OR (:interiorThings IS NULL OR tp.interior_things = :interiorThings) " +
+            "OR (:movie IS NULL OR tp.movie = :movie) " +
+            "OR (:musical IS NULL OR tp.musical = :musical) " +
+            "OR (:sports IS NULL OR tp.sports = :sports) " +
+            "OR (:game IS NULL OR tp.game = :game) " +
+            "OR (:itTech IS NULL OR tp.it_tech = :itTech) " +
+            "OR (:kpop IS NULL OR tp.kpop = :kpop) " +
+            "OR (:alcohol IS NULL OR tp.alcohol = :alcohol) " +
+            "OR (:animalPlant IS NULL OR tp.animal_plant = :animalPlant) " +
+            "OR (:etc IS NULL OR tp.etc = :etc)) " +
+            "AND ( " +
+            "OR (:market IS NULL OR pp.market = :market) " +
+            "OR (:display IS NULL OR pp.display = :display) " +
+            "OR (:experience IS NULL OR pp.experience = :experience))",
             countQuery = "SELECT COUNT(*) FROM popups p " +
                     "LEFT JOIN prefered_popup pp ON p.prefered_id = pp.id " +
                     "LEFT JOIN taste_popup tp ON p.taste_id = tp.id " +
                     "WHERE (:text IS NULL OR :text = '' OR MATCH(p.name, p.introduce) AGAINST (:text IN BOOLEAN MODE)) " +
                     "AND p.operation_status = :oper " +
-                    "AND (:fashionBeauty IS NULL OR tp.fashion_beauty = :fashionBeauty) " +
-                    "AND (:characters IS NULL OR tp.characters = :characters) " +
-                    "AND (:foodBeverage IS NULL OR tp.food_beverage = :foodBeverage) " +
-                    "AND (:webtoonAni IS NULL OR tp.webtoon_ani = :webtoonAni) " +
-                    "AND (:interiorThings IS NULL OR tp.interior_things = :interiorThings) " +
-                    "AND (:movie IS NULL OR tp.movie = :movie) " +
-                    "AND (:musical IS NULL OR tp.musical = :musical) " +
-                    "AND (:sports IS NULL OR tp.sports = :sports) " +
-                    "AND (:game IS NULL OR tp.game = :game) " +
-                    "AND (:itTech IS NULL OR tp.it_tech = :itTech) " +
-                    "AND (:kpop IS NULL OR tp.kpop = :kpop) " +
-                    "AND (:alcohol IS NULL OR tp.alcohol = :alcohol) " +
-                    "AND (:animalPlant IS NULL OR tp.animal_plant = :animalPlant) " +
-                    "AND (:market IS NULL OR pp.market = :market) " +
-                    "AND (:display IS NULL OR pp.display = :display) " +
-                    "AND (:experience IS NULL OR pp.experience = :experience)" +
-                    "AND (:etc IS NULL OR tp.etc = :etc)",
+                    "AND (" +
+                    "(:fashionBeauty IS NULL OR tp.fashion_beauty = :fashionBeauty) " +
+                    "OR (:characters IS NULL OR tp.characters = :characters) " +
+                    "OR (:foodBeverage IS NULL OR tp.food_beverage = :foodBeverage) " +
+                    "OR (:webtoonAni IS NULL OR tp.webtoon_ani = :webtoonAni) " +
+                    "OR (:interiorThings IS NULL OR tp.interior_things = :interiorThings) " +
+                    "OR (:movie IS NULL OR tp.movie = :movie) " +
+                    "OR (:musical IS NULL OR tp.musical = :musical) " +
+                    "OR (:sports IS NULL OR tp.sports = :sports) " +
+                    "OR (:game IS NULL OR tp.game = :game) " +
+                    "OR (:itTech IS NULL OR tp.it_tech = :itTech) " +
+                    "OR (:kpop IS NULL OR tp.kpop = :kpop) " +
+                    "OR (:alcohol IS NULL OR tp.alcohol = :alcohol) " +
+                    "OR (:animalPlant IS NULL OR tp.animal_plant = :animalPlant) " +
+                    "OR (:etc IS NULL OR tp.etc = :etc)) " +
+                    "AND ( " +
+                    "OR (:market IS NULL OR pp.market = :market) " +
+                    "OR (:display IS NULL OR pp.display = :display) " +
+                    "OR (:experience IS NULL OR pp.experience = :experience))",
             nativeQuery = true)
     Page<Popup> findByTextInNameOrIntroduce(String text, Pageable pageable,
                                             Boolean market, Boolean display, Boolean experience,
