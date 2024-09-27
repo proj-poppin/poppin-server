480dff7cf3a9d96edd3b653ba3505a4cadbea0d6
jun6292 <c68254@gmail.com>
2024-09-28 03:00:57 +0900

    fix: 팝업/후기 신고 api 수정

--- src/main/java/com/poppin/poppinserver/report/dto/report/request/CreateReviewReportDto.java
+++ src/main/java/com/poppin/poppinserver/report/dto/report/request/CreateReviewReportDto.java
@@ -3,7 +3,7 @@ package com.poppin.poppinserver.report.dto.report.request;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateReviewReportDto(
-        @NotNull(message = "신고 내용을 입력하세요.")
-        String content
+        @NotNull(message = "신고 내용을 입력하세요.") String content,
+        @NotNull(message = "reviewId 누락") Long reviewId
 ) {
 }
--- src/main/java/com/poppin/poppinserver/report/service/ReportService.java
+++ src/main/java/com/poppin/poppinserver/report/service/ReportService.java
@@ -1,24 +1,23 @@
 package com.poppin.poppinserver.report.service;
 
-import com.poppin.poppinserver.popup.repository.PopupRepository;
-import com.poppin.poppinserver.report.dto.report.request.CreatePopupReportDto;
-import com.poppin.poppinserver.report.dto.report.request.CreateReviewReportDto;
 import com.poppin.poppinserver.core.exception.CommonException;
 import com.poppin.poppinserver.core.exception.ErrorCode;
 import com.poppin.poppinserver.popup.domain.Popup;
+import com.poppin.poppinserver.popup.repository.PopupRepository;
 import com.poppin.poppinserver.report.domain.ReportPopup;
 import com.poppin.poppinserver.report.domain.ReportReview;
+import com.poppin.poppinserver.report.dto.report.request.CreatePopupReportDto;
+import com.poppin.poppinserver.report.dto.report.request.CreateReviewReportDto;
 import com.poppin.poppinserver.report.repository.ReportPopupRepository;
 import com.poppin.poppinserver.report.repository.ReportReviewRepository;
 import com.poppin.poppinserver.review.domain.Review;
 import com.poppin.poppinserver.review.repository.ReviewRepository;
 import com.poppin.poppinserver.user.domain.User;
 import com.poppin.poppinserver.user.repository.UserRepository;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -31,10 +30,10 @@ public class ReportService {
     private final ReportPopupRepository reportPopupRepository;
     private final PopupRepository popupRepository;
 
-    public void createReviewReport(Long userId, Long reviewId, CreateReviewReportDto createReviewReportDto) {
+    public void createReviewReport(Long userId, CreateReviewReportDto createReviewReportDto) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
-        Review review = reviewRepository.findById(reviewId)
+        Review review = reviewRepository.findById(createReviewReportDto.reviewId())
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_REVIEW));
         ReportReview reportReview = ReportReview.builder()
                 .reporterId(user)
@@ -46,10 +45,10 @@ public class ReportService {
         reportReviewRepository.save(reportReview);
     }
 
-    public void createPopupReport(Long userId, Long popupId, CreatePopupReportDto createPopupReportDto) {
+    public void createPopupReport(Long userId, CreatePopupReportDto createPopupReportDto) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
-        Popup popup = popupRepository.findById(popupId)
+        Popup popup = popupRepository.findById(createPopupReportDto.popupId())
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_POPUP));
         ReportPopup reportPopup = ReportPopup.builder()
                 .reporterId(user)
