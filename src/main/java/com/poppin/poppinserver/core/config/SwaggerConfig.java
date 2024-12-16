e0e2a78e92676bd1a14b6ca33c210ecbbdbb6450
Gouyeon Chung <gouyeonch@naver.com>
2024-12-17 02:25:31 +0900

    FEAT : 스웨거 요청 인증버튼 활성화

--- src/main/java/com/poppin/poppinserver/core/config/SwaggerConfig.java
+++ src/main/java/com/poppin/poppinserver/core/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.poppin.poppinserver.core.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +14,13 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT") // JWT 토큰 포맷 지정
+                                .name("Authorization"))) // 헤더 이름 지정
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth")) // Security 설정 추가
                 .info(apiInfo());
     }
 
