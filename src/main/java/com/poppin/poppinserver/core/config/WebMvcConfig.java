153388261cdff319f9062f9bc3c2d6aadfc1fa21
sakang <rogersddt@naver.com>
2025-01-03 02:11:53 +0900

    Fix: cors Patch Mapping 허용

--- src/main/java/com/poppin/poppinserver/core/config/WebMvcConfig.java
+++ src/main/java/com/poppin/poppinserver/core/config/WebMvcConfig.java
@@ -38,7 +38,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "https://www.bubble-poppin.com",
                         "http://43.202.82.111") // vite 로컬 주소, 운영 환경, 개발 환경
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 }
