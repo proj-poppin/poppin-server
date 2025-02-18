580ec4608c595fb1b963b30895c7628389612fc1
Gouyeon Chung <gouyeonch@naver.com>
2025-02-18 22:12:30 +0900

    FEAT : security class log

--- src/main/java/com/poppin/poppinserver/core/security/handler/JwtAccessDeniedHandler.java
+++ src/main/java/com/poppin/poppinserver/core/security/handler/JwtAccessDeniedHandler.java
@@ -5,15 +5,19 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class JwtAccessDeniedHandler extends AbstractFailureResponse implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.info("JwtAccessDeniedHandler");
         setFailureResponse(response, ErrorCode.ACCESS_DENIED_ERROR);
     }
 }
--- src/main/java/com/poppin/poppinserver/core/security/handler/JwtAuthEntryPoint.java
+++ src/main/java/com/poppin/poppinserver/core/security/handler/JwtAuthEntryPoint.java
@@ -5,15 +5,19 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class JwtAuthEntryPoint extends AbstractFailureResponse implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
+        log.info("JwtAuthEntryPoint");
         setFailureResponse(response, ErrorCode.INVALID_TOKEN_ERROR);
     }
 }
