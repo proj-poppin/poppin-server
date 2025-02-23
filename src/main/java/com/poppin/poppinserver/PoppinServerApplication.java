05954c30ba5ca295e8446ceb2c469c52a99c6239
sakang <rogersddt@naver.com>
2025-02-24 03:11:52 +0900

    Chore: 빌드 오류 해결 22

--- src/main/java/com/poppin/poppinserver/PoppinServerApplication.java
+++ src/main/java/com/poppin/poppinserver/PoppinServerApplication.java
@@ -1,11 +1,14 @@
 package com.poppin.poppinserver;
 
+import com.poppin.poppinserver.core.config.AwsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableScheduling
+@EnableConfigurationProperties(AwsProperties.class)
 @SpringBootApplication
 public class PoppinServerApplication {
 
