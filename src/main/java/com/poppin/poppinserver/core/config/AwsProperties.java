dcb7aa8b061415098f51df72e7525f30ebd7ebd7
sakang <rogersddt@naver.com>
2025-02-24 02:22:33 +0900

    Chore: 빌드 오류 해결 14

---
+++ src/main/java/com/poppin/poppinserver/core/config/AwsProperties.java
@@ -0,0 +1,28 @@
+package com.poppin.poppinserver.core.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "cloud.aws")
+public class AwsProperties {
+    private Credentials credentials;
+    private Region region;
+
+    @Getter
+    @Setter
+    public static class Credentials {
+        private String accessKey;
+        private String secretKey;
+    }
+
+    @Getter
+    @Setter
+    public static class Region {
+        private String staticRegion;
+    }
+}
--- src/main/java/com/poppin/poppinserver/core/config/S3Config.java
+++ src/main/java/com/poppin/poppinserver/core/config/S3Config.java
@@ -1,27 +1,29 @@
 package com.poppin.poppinserver.core.config;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-    @Value("${cloud.aws.region.static}")
-    private String region;
+
+    private final AwsProperties awsProperties;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+    public AmazonS3 amazonS3Client() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                awsProperties.getCredentials().getAccessKey(),
+                awsProperties.getCredentials().getSecretKey()
+        );
+
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(awsProperties.getRegion().getStaticRegion())
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
