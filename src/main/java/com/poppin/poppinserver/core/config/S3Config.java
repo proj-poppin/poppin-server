56393aab36fdf8976f140ac2f325a2a0279fbe7c
sakang <rogersddt@naver.com>
2025-02-24 02:31:21 +0900

    Chore: 빌드 오류 해결 15

--- src/main/java/com/poppin/poppinserver/core/config/S3Config.java
+++ src/main/java/com/poppin/poppinserver/core/config/S3Config.java
@@ -3,7 +3,7 @@ package com.poppin.poppinserver.core.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -16,13 +16,13 @@ public class S3Config {
     private final AwsProperties awsProperties;
 
     @Bean
-    public AmazonS3 amazonS3Client() {
+    public AmazonS3Client amazonS3Client() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
                 awsProperties.getCredentials().getAccessKey(),
                 awsProperties.getCredentials().getSecretKey()
         );
 
-        return AmazonS3ClientBuilder.standard()
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(awsProperties.getRegion().getStaticRegion())
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
