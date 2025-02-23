14229b6b2d915e563f6cf1351ab8ec5bf9f360ce
sakang <rogersddt@naver.com>
2025-02-24 02:44:10 +0900

    Chore: 빌드 오류 해결 17

--- src/main/java/com/poppin/poppinserver/core/config/AwsProperties.java
+++ src/main/java/com/poppin/poppinserver/core/config/AwsProperties.java
@@ -4,11 +4,11 @@ import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "cloud.aws")
 public class AwsProperties {
     private Credentials credentials;
